f08a52ec7c682ed34e063e23c2794979d8788a57
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2021-06-15 08:45:15 +0900

    投稿詳細 css 追加

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,12 +1,17 @@
 <template>
-  <section class="container">
-    <h1>{{ post.fields.title }}</h1>
-    <v-row>
-      <v-col>
-        <div v-html="toHtmlString(post.fields.body)"></div>
-      </v-col>
-    </v-row>
-  </section>
+   <div>
+      <Header />
+      <section class="post-detail">
+        <div class="image">
+          <img src="profile_test.jpeg">
+        </div>
+        <p class="username">Takenari.Y</p>
+        <p class="date">{{ post.fields.openedAt }}</p>
+        <br>
+        <h1 class="title">{{ post.fields.title }}</h1>
+        <p class="content" v-html="toHtmlString(post.fields.body)"></p>
+      </section>
+   </div>
 </template>
 <script>
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
@@ -24,7 +29,11 @@ export default {
   methods: {
     toHtmlString(obj) {
       return documentToHtmlString(obj)
+    },
+    window:onload = function() {
+      alert("tets")
     }
-  }
+  },
+
 }
 </script>
